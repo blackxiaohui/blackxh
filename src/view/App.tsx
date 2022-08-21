d5528bc5865ac0706600590d1ebc3f973ca8601f
布莱克小辉 <blackxiaohui@163.com>
2022-08-21 09:50:53 +0800

    init project

---
+++ src/view/App.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import "./App.less";
+
+
+export const App: FC = () => {
+
+    return (
+        <div className="app">
+            <div className="main">
+                布莱克小辉的个人网站
+            </div>
+            <div className="main">
+                搭建中......
+            </div>
+        </div>
+    );
+}
