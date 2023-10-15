eb216f74ac579c37da2ea244aea6122ec0166725
布莱克小辉 <blackxiaohui@163.com>
2023-10-15 20:42:12 +0800

    fix: 追加日志

--- src/conf/log.ts
+++ src/conf/log.ts
@@ -6,6 +6,10 @@ export interface LogItem {
 
 export const logList: LogItem[] = [
     {
+        time: "2023-10-15",
+        commit: "新增五子棋小游戏",
+    },
+    {
         time: "2022-10-03",
         commit: "新增图片取色器",
     },
